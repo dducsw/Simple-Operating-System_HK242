148761069b0d345c376eb57e9e206a11e76af22a
DucMinh2211 <minh.ha02211050@hcmut.edu.vn>
2025-04-16 23:41:53 +0700

    Update mm-vm.c's comment 1

--- ossim_sierra/src/mm-vm.c
+++ ossim_sierra/src/mm-vm.c
@@ -56,18 +56,23 @@ struct vm_rg_struct *get_vm_area_node_at_brk(struct pcb_t *caller, int vmaid,
   struct vm_rg_struct *newrg;
   /* TODO retrive current vma to obtain newrg, current comment out due to
    * compiler redundant warning*/
+  // Get the virtual memory based on the provided ID
   struct vm_area_struct *cur_vma = get_vma_by_num(caller->mm, vmaid);
   if (cur_vma == NULL)
     return NULL;
 
+  // Allocated memory for the new virtual memory region
   newrg = malloc(sizeof(struct vm_rg_struct));
-  int start_address = cur_vma->sbrk;
-  int end_address = start_address + size;
+  // Calculate the start and end address of new region
+  int start_address =
+      cur_vma->sbrk; // start = break point (end of allocated heap) of VMA
+  int end_address = start_address + size; // end = start + requested size
 
   /* TODO: update the newrg boundary
   // newrg->rg_start = ...
   // newrg->rg_end = ...
   */
+  // Set the start and end address of new region
   newrg->rg_start = start_address;
   newrg->rg_end = end_address;
 
@@ -88,16 +93,21 @@ int validate_overlap_vm_area(struct pcb_t *caller, int vmaid, int vmastart,
   /* TODO validate the planned memory area is not overlapped */
   if (caller == NULL)
     return -1;
+  // Get virtual memory area (vma) based on provided id
   struct vm_area_struct *cur_vma = get_vma_by_num(caller->mm, vmaid);
+  // Iterate through all vma based on process's mem map
   while (vma != NULL) {
+    // Check if current vma is not the target vma
     if (vma->vm_id != vmaid) {
       if (vmaend > vma->vm_start && vmastart < vma->vm_end && vma != cur_vma) {
-        return 1; // memory area overlapped
+        return 1; // Memory area overlapped
       }
     }
+    // Move to next vma in the list
     vma = vma->vm_next;
   }
 
+  // No overlapped found
   return 0;
 }
 
@@ -108,13 +118,19 @@ int validate_overlap_vm_area(struct pcb_t *caller, int vmaid, int vmastart,
  *
  */
 int inc_vma_limit(struct pcb_t *caller, int vmaid, int inc_sz) {
+  // Allocate memory for a new virtual memory (vm) region
   struct vm_rg_struct *newrg = malloc(sizeof(struct vm_rg_struct));
+  // Align increment size to the page size.
   int inc_amt = PAGING_PAGE_ALIGNSZ(inc_sz);
+  // Calculate the number of pages required for the increment
   int incnumpage = inc_amt / PAGING_PAGESZ;
+  // Get vm region node at break point for requested increment
   struct vm_rg_struct *area =
       get_vm_area_node_at_brk(caller, vmaid, inc_sz, inc_amt);
+  // Get vm area (vma) based on provided id
   struct vm_area_struct *cur_vma = get_vma_by_num(caller->mm, vmaid);
 
+  // Store old end address of the vma before increment
   int old_end = cur_vma->vm_end;
 
   /*Validate overlap of obtained region */
@@ -124,14 +140,17 @@ int inc_vma_limit(struct pcb_t *caller, int vmaid, int inc_sz) {
   /* TODO: Obtain the new vm area based on vmaid */
   // cur_vma->vm_end...
   //  inc_limit_ret...
+  //  Increase the end address and break point of the vma by requested size
   cur_vma->vm_end += inc_sz;
   cur_vma->sbrk += inc_sz;
 
+  // Map the new reversed vm space to physical RAM.
   if (vm_map_ram(caller, area->rg_start, area->rg_end, old_end, incnumpage,
                  newrg) < 0) {
     return -1; /* Map the memory to MEMRAM */
   }
 
+  // Increase vma limit successful
   return 0;
 }
 
